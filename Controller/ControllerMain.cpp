7e42e48919cc6f830a63845ee4d3d8fe1307a8b0
Frank Buckley <frank@dsegroup.org>
2021-06-06 20:29:10 +0100

    Update ControllerMain.cpp

--- Controller/ControllerMain.cpp
+++ Controller/ControllerMain.cpp
@@ -3,12 +3,41 @@
 #include <stdlib.h>
 #include <strsafe.h>
 #include <detour/include/detours.h>
-#include <gl/GL.h>
-#include <gl/GLU.h>
-#include <glfw/include/GLFW/glfw3.h>
+//#include <gl/GL.h>
+//#include <gl/GLU.h>
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+// About Desktop OpenGL function loaders:
+//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
+//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
+//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h>            // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>            // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>          // Initialize with gladLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+#include <../include/glad/include/glad/gl.h>		// Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#else
+#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
+
+#include <glfw/include/GLFW/glfw3.h>
 
 static BOOL(* TrueGlSwapLayerBuffers)(HDC dc, UINT params);
 static bool fly = false;
@@ -22,53 +51,53 @@ void GlSwapLayerBuffers(
 {
 	OutputDebugString(L"[Controller] GlSwapLayerBuffers in Controller.\n");
 
-	if (window)
-	{
-		OutputDebugString(L"[Controller] Calling glfwPollEvents...\n");
+	//if (window)
+	//{
+		//OutputDebugString(L"[Controller] Calling glfwPollEvents...\n");
 
 		//glfwPollEvents();
 
 		// Start the Dear ImGui frame
 
-		OutputDebugString(L"[Controller] Calling ImGui_ImplOpenGL3_NewFrame...\n");
+		//OutputDebugString(L"[Controller] Calling ImGui_ImplOpenGL3_NewFrame...\n");
 
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
+		//ImGui_ImplOpenGL3_NewFrame();
+		//ImGui_ImplGlfw_NewFrame();
 
-		OutputDebugString(L"[Controller] Started ImGui frame.\n");
+		//OutputDebugString(L"[Controller] Started ImGui frame.\n");
 
-		ImGui::NewFrame();
-		{
-			ImGui::Begin("BlackTeal Client");
+		//ImGui::NewFrame();
+		//{
+		//	ImGui::Begin("BlackTeal Client");
 
-			ImGui::Checkbox("Fly", &fly);
-			ImGui::SameLine();
+		//	ImGui::Checkbox("Fly", &fly);
+		//	ImGui::SameLine();
 
 
-			if (ImGui::Button("Unload Cheat"))
-				unload = true;
+		//	if (ImGui::Button("Unload Cheat"))
+		//		unload = true;
 
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			ImGui::End();
-		}
+		//	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		//	ImGui::End();
+		//}
 
-		// Rendering
-		ImGui::Render();
+		//// Rendering
+		//ImGui::Render();
 
-		OutputDebugString(L"[Controller] Called ImGui::Render.\n");
+		//OutputDebugString(L"[Controller] Called ImGui::Render.\n");
 
-		int display_w, display_h;
+		//int display_w, display_h;
 
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		//glfwGetFramebufferSize(window, &display_w, &display_h);
+		//glViewport(0, 0, display_w, display_h);
+		//glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		//glClear(GL_COLOR_BUFFER_BIT);
+		//ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		glfwSwapBuffers(window);
-	}
+		//glfwSwapBuffers(window);
+	//}
 
-	//TrueGlSwapLayerBuffers(dc, params);
+	TrueGlSwapLayerBuffers(dc, params);
 }
 
 static void glfw_error_callback(int error, const char* description)
@@ -76,54 +105,51 @@ static void glfw_error_callback(int error, const char* description)
 	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-static void InitImGui()
+DWORD WINAPI InitImGui(LPVOID lpParam)
 {
 	// Setup window
 	glfwSetErrorCallback(glfw_error_callback);
+
 	if (!glfwInit())
-		return;
+		return FALSE;
 
 	const char* glsl_version = "#version 130";
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
 	// Create window with graphics context
 	window = glfwCreateWindow(800, 600, "Hello World :D", NULL, NULL);
-	//if (window == NULL)
-	//    return 1;
+
+	if (window == NULL)
+	{
+		return FALSE;
+	}
 
 	glfwMakeContextCurrent(window);
-	glfwSwapInterval(0);
+	glfwSwapInterval(1);
 
 	// Initialize OpenGL loader
 
-#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-	bool err = gl3wInit() != 0;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-	bool err = glewInit() != GLEW_OK;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-	bool err = gladLoadGL() == 0;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING)
-	bool err = false;
-	glbinding::initialize([](const char* name) { return (glbinding::ProcAddress)glfwGetProcAddress(name); });
-#else
-	bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
-#endif
+	bool err = gladLoadGL(glfwGetProcAddress) == 0;
+
 	if (err)
 	{
-		fprintf(stderr, "Failed to initialize OpenGL loader!\n");
-		return;
+		OutputDebugString(L"Failed to initialize OpenGL loader.\n");
+		return FALSE;
 	}
 
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
-	io = ImGui::GetIO(); (void)io;
+	//io = ImGui::GetIO(); (void)io;
 
-	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsDark();
 
-	// Setup Platform/Renderer bindings
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init(glsl_version);
+	//// Setup Platform/Renderer bindings
+	// ImGui_ImplGlfw_InitForOpenGL(window, true);
+	// ImGui_ImplOpenGL3_Init(glsl_version);
+
+	return TRUE;
 }
 
 BOOL WINAPI DllMain(
@@ -134,6 +160,7 @@ BOOL WINAPI DllMain(
 	OutputDebugString(L"[Controller] DllMain called.\n");
 
 	HMODULE openGlModule = {};
+	LPDWORD threadId = {};
 
 	if (DetourIsHelperProcess()) {
 		return TRUE;
@@ -191,7 +218,13 @@ BOOL WINAPI DllMain(
 
 		OutputDebugString(L"[Controller] DetourAttach completed.\n");
 
-		InitImGui();
+		CreateThread(
+			NULL,           // default security attributes
+			0,              // use default stack size  
+			InitImGui,      // thread function name
+			NULL,			// argument to thread function 
+			0,              // use default creation flags 
+			threadId);		// returns the thread identifier 
 
 		break;
 
